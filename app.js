8d4154d34767c1d729aa1b0c874305b508969b23
Youness <elkhatabi.youness@gmail.com>
2020-03-30 14:36:39 +0200

    add connection to MongoDB Atlas

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
+// MONGODB PW: REdZ170gKQpf8KS8
+// MONGODB CONNEXION: mongodb+srv://will:<password>@cluster0-lh2dz.mongodb.net/test?retryWrites=true&w=majority
+
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();  
+
+mongoose.connect('mongodb+srv://will:REdZ170gKQpf8KS8@cluster0-lh2dz.mongodb.net/test?retryWrites=true&w=majority')
+.then(() => {
+    console.log('Successfully connected to MongoDB Atlas!');
+})
+.catch((error) => {
+    console.log('Unable to connect to MongoDB Altas!');
+    console.error(error);
+});
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
