c69ca10e47cf7d2bca27e95d4de1e40d70e68271
Youness <elkhatabi.youness@gmail.com>
2020-03-31 23:21:47 +0200

    add deleting a thing

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // MONGODB PW: REdZ170gKQpf8KS8
-// MONGODB CONNEXION: mongodb+srv://will:<password>@cluster0-lh2dz.mongodb.net/test?retryWrites=true&w=majority
+// MONGODB CONNEXION: mongodb+srv://will:<password>@cluster0-lh2dz.mongodb.net/test?retryWrites=true&w=majority 
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -60,6 +60,22 @@ app.get('/api/stuff/:id', (req, res, next) => {
     ).catch(
         (error) => {
             res.status(404).json({
+                error: error
+            });
+        }
+    );
+});
+
+app.delete('/api/stuff/:id', (req, res, next) => {
+    Thing.deleteOne({_id: req.params.id}).then(
+        () => {
+            res.status(200).json({
+                message: 'Deleted!'
+            });
+        }
+    ).catch(
+        (error) => {
+            res.status(400).json({
                 error: error
             });
         }
